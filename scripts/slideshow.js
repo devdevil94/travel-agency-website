175b61c7c03e1461c4f49131641060412fb4fddd
devdevil94 <saudd2gh@gmail.com>
2018-07-24 12:12:41 +0800

    minor fixes

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("slides");
+  var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
